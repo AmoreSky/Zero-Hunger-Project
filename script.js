55bff95b664efc2db52a23ef804971c02b72b8e5
Adeoye Anthonia Oluwarantimi <toniaoluwarantimi@gmail.com>
2025-11-26 15:07:58 +0100

    Add HTML, CSS, and JavaScript files for donation and sign-in features; remove unused homepage files

---
+++ script.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCMDYlFX-iCBtWM5Feqc1Sg72lgr2AlAus",
+    authDomain: "zero-hunger-project-c92ea.firebaseapp.com",
+    databaseURL: "https://zero-hunger-project-c92ea-default-rtdb.firebaseio.com",
+    projectId: "zero-hunger-project-c92ea",
+    storageBucket: "zero-hunger-project-c92ea.firebasestorage.app",
+    messagingSenderId: "242178592978",
+    appId: "1:242178592978:web:cb8b5439c5f59982be1d0b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
